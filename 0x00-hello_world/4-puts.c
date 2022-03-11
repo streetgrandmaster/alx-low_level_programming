b7ca712dc51c4670ae53e7638ceb428bd0c28ebd
LAWRENCE <laurielawi12@gmail.com>
2022-03-11 05:53:52 +0300

    C program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return:  0 if the program is run successfully else return 1
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

684eb26e741b45e67dff3672e469ba097b9ffcfb
LAWRENCE <laurielawi12@gmail.com>
2022-04-27 05:57:27 +0300

    returns the number of elements in a linked listint_t

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t nnodes = 0;
+	size_t nodes = 0;
 
-    while (h != NULL)
-    {
-    h = h->next;
-    nnodes++;
-    }
-    return (nnodes);
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }

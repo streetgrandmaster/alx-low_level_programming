1ce7017a254d5c582b6b575dce0472f94bbb93b7
LAWRENCE <laurielawi12@gmail.com>
2022-04-27 05:28:32 +0300

    deletes the head node of a listint_t

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,11 +8,11 @@
  */
 void free_listint(listint_t *head)
 {
-listint_t *temp;
+    listint_t *temp;
 
-while ((temp = head) != NULL)
-{
-head = head->next;
-free(temp);
-}
+    while ((temp = head) != NULL)
+    {
+        head = head->next;
+        free(temp);
+    }
 }

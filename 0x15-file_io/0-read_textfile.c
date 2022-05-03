be962741239dd0cf4d4e02b9e8755efc6a2ab703
LAWRENCE <laurielawi12@gmail.com>
2022-05-03 18:00:17 +0300

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,45 @@
 #include "main.h"
-
 /**
- *  read_textfile - read a certain size and prints to std output
- * filename: file to read from
- * letters: size to read
- * return: actual size read and printed
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ *
+ * Return: the number of letters printed, or 0 if it failed
  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+int s, t;
+char *buf;
 
-ssize_t read_textfile(const char *filename, size_t letters){
-    int file_desc; // file descriptor
-    ssize_t n_read, n_wrote;
-    char buffer;
+if (!filename)
+return (0);
 
-    if (filename == NULL)
-        return (0);
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+return (0);
 
-    // open
-    file_desc = open(filename, O_RDONLY);
-    if (file_desc == -1)
-        return (0);
+buf = malloc(sizeof(char) * letters);
+if (!buf)
+return (0);
 
-    // malloc buffer
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+s = read(fd, buf, letters);
+if (s < 0)
+{
+free(buf);
+return (0);
+}
+buf[s] = '\0';
 
-    // read
-    n_read = read(file_desc, buffer, letters);
-    if (n_read == -1){
-        free(buffer);
-        close(file_desc);
-        return (0);
-    }
+close(fd);
 
-    // write
-    n_wrote = write(STDOUT_FILENO, buffer, n_read);
-    if (n_wrote == -1){
-        free(buffer);
-        close(file_desc);
-        return (0);
-    }
+t = write(STDOUT_FILENO, buf, s);
+if (t < 0)
+{
+free(buf);
+return (0);
+}
 
-    close(file_desc);
-    return (n_read);
-
+free(buf);
+return (t);
 }
